5a98008a014bf560534fa4cc54932e1337fb595c
cdliuxun <524270348@qq.com>
2020-12-06 12:50:44 +0800

    第3次使用git提交.....

--- src/com/vtradex/net/Main.java
+++ src/com/vtradex/net/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("我是第一个git项目");
         System.out.println("我是第一个git项目");
+        System.out.println("我是第3个git项目");
     }
 }
